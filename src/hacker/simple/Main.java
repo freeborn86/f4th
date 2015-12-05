a93b494fa4af3d97237059cc5ba7cff32dbf8fab
balazs gabonyi <gabonyibalazs@gmail.com>
2015-12-05 16:33:22 +0100

    Changed main to loop multiple hacks while exit is input

--- src/hacker/simple/Main.java
+++ src/hacker/simple/Main.java
@@ -5,8 +5,16 @@ import java.io.IOException;
 public class Main {
 
 	public static void main(String[] args) throws IOException {
-		SimpleHacker sh = Console.getWords();
-		sh.hack();
+		while (true) {
+			SimpleHacker sh = Console.getWords();
+			sh.hack();
+			String exit = "";
+			System.out.println("Type exit + ENTER to exit");
+			exit = Console.readUpperFromKeyboard();
+			if (exit.equals("EXIT")) {
+				break;
+			}
+		}
 		Console.keyboard.close();
 		Console.br.close();
 	}
